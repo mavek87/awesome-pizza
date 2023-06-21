ee017e552b9e48e56b097f1c2949b7aadcae911b
matteo <matteo.veroni@giuffre.it>
2023-06-21 16:53:51 +0200

    create due chiamate get in OrderController

--- src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
+++ src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
@@ -2,20 +2,28 @@ package com.matteoveroni.awesomepizza.controllers;
 
 import com.matteoveroni.awesomepizza.model.Order;
 import com.matteoveroni.awesomepizza.services.OrdersService;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("orders")
+@RequestMapping(value = "/orders")
 public class OrderController {
 
     private final OrdersService ordersService;
 
     public OrderController(OrdersService ordersService) {
         this.ordersService = ordersService;
+    }
+
+    @GetMapping(value = "/", produces = "application/json")
+    public List<Order> getOrder() {
+        return ordersService.getAllOrders();
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
--- src/main/java/com/matteoveroni/awesomepizza/model/Order.java
+++ src/main/java/com/matteoveroni/awesomepizza/model/Order.java
@@ -1,17 +1,8 @@
 package com.matteoveroni.awesomepizza.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.matteoveroni.awesomepizza.model.converters.OrderStateEnumConverter;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.Date;
 import java.util.List;
@@ -35,12 +26,13 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
-    @OneToMany(cascade = {CascadeType.ALL})
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
     @JoinTable(
             name = "order_items",
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "order_item_id")
     )
+    @JsonIgnore
     private List<OrderItem> orderItems;
     @Column(name = "order_state")
     @Convert(converter = OrderStateEnumConverter.class)
