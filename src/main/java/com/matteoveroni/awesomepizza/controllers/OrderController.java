4892ec426f993aac1d3a101bb6ba12441620bc4e
veroni <matteo.veroni@giuffre.it>
2023-06-21 19:37:05 +0200

    refactoring

--- src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
+++ src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
@@ -1,11 +1,15 @@
 package com.matteoveroni.awesomepizza.controllers;
 
+import com.matteoveroni.awesomepizza.model.Order;
 import com.matteoveroni.awesomepizza.model.dto.OrderDTO;
 import com.matteoveroni.awesomepizza.services.OrdersService;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +23,22 @@ public class OrderController {
         this.ordersService = ordersService;
     }
 
-    @GetMapping(value = "/", produces = "application/json")
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<OrderDTO> getOrder() {
         return ordersService.getAllOrders();
     }
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OrderDTO> getOrder(@PathVariable Long id) {
+//        ordersService.registerNewOrder()
         return ResponseEntity.of(ordersService.getOrder(id));
     }
+
+//    @PostMapping(
+//            value = "/register_order",
+//            consumes = MediaType.APPLICATION_JSON_VALUE
+//    )
+//    public void Long registerNewOrder(@RequestBody Order orderDTO) {
+//        return 1L;
+//    }
 }
