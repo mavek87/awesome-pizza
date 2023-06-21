f4e72f5887b7112041f3a5746361fea1ade46c34
matteo <matteo.veroni@giuffre.it>
2023-06-21 16:46:14 +0200

    iniziato a creare controller

--- src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
+++ src/main/java/com/matteoveroni/awesomepizza/controllers/OrderController.java
@@ -1,9 +1,25 @@
 package com.matteoveroni.awesomepizza.controllers;
 
+import com.matteoveroni.awesomepizza.model.Order;
+import com.matteoveroni.awesomepizza.services.OrdersService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("orders")
 public class OrderController {
 
+    private final OrdersService ordersService;
 
+    public OrderController(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Order> getOrder(@PathVariable Long id) {
+        return ResponseEntity.of(ordersService.getOrder(id));
+    }
 }
